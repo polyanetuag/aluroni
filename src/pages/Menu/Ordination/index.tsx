276d33a091e92aa96709841075032fb3e876f985
Polyane Tuag <pollyportugues@hotmail.com>
2022-12-01 07:26:17 -0300

    feat: configuration select

--- src/pages/Menu/Ordination/index.tsx
+++ src/pages/Menu/Ordination/index.tsx
@@ -1,11 +1,21 @@
 import styles from './Ordination.module.scss';
 import options from './options.json';
+import { useState } from 'react';
+import classNames from 'classnames';
 
 export default function Ordination() {
+    const [open, setOpen] = useState(false);
     return (
-        <button className={styles.ordination}>
+        <button 
+        className={styles.ordination} 
+        onClick={() => setOpen(!open)}
+        onBlur={() => setOpen(false)}
+        >
             <span>Ordenar por</span>
-            <div className={styles.ordination__options}>
+            <div className={classNames({
+                [styles.ordination__options] : true,
+                [styles["ordination__options--active"]] : open
+            })}>
                 {options.map((option) => (
                     <div className={styles.ordination__option} key={option.value}>{option.nome}</div>
                 ))}
