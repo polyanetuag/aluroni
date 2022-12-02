bd139249a1b03ab362da77fb604c1100e600993d
Polyane Tuag <pollyportugues@hotmail.com>
2022-12-02 08:44:58 -0300

    feat: add icon ordination

--- src/pages/Menu/Ordination/index.tsx
+++ src/pages/Menu/Ordination/index.tsx
@@ -2,6 +2,8 @@ import styles from './Ordination.module.scss';
 import options from './options.json';
 import { useState } from 'react';
 import classNames from 'classnames';
+import {MdKeyboardArrowUp, MdKeyboardArrowDown} from 'react-icons/md';
+
 
 export default function Ordination() {
     const [open, setOpen] = useState(false);
@@ -12,6 +14,7 @@ export default function Ordination() {
         onBlur={() => setOpen(false)}
         >
             <span>Ordenar por</span>
+            { open ? <MdKeyboardArrowUp size={20}/> : <MdKeyboardArrowDown size={20}/> }
             <div className={classNames({
                 [styles.ordination__options] : true,
                 [styles["ordination__options--active"]] : open
