08ba1d11acb34d6978e55940f4fc5029d34bb0bf
Polyane Tuag <pollyportugues@hotmail.com>
2022-12-02 09:00:52 -0300

    feat: add props ordination

--- src/pages/Menu/index.tsx
+++ src/pages/Menu/index.tsx
@@ -8,6 +8,7 @@ import Ordination from './Ordination';
 export default function Menu() {
 const [search, setSearch] = useState("");
 const [filter, setFilter] = useState<number | null>(null);
+const [ordination, setOrdination] = useState('');
 
   return (
     <main>
@@ -22,7 +23,7 @@ const [filter, setFilter] = useState<number | null>(null);
           <Search search={search} setSearch={setSearch} />
           <div className={styles.menu__filters}>
             <Filters filter={filter} setFilter={setFilter} />
-            <Ordination />
+            <Ordination ordination={ordination} setOrdination={setOrdination} />
           </div>
         </section>
     </main>
