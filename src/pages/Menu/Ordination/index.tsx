8fbc983a0c7078d954aa74d3c67cca12ffed1415
Polyane Tuag <pollyportugues@hotmail.com>
2022-12-02 09:01:17 -0300

    feat: control ordination

--- src/pages/Menu/Ordination/index.tsx
+++ src/pages/Menu/Ordination/index.tsx
@@ -3,24 +3,36 @@ import options from './options.json';
 import { useState } from 'react';
 import classNames from 'classnames';
 import {MdKeyboardArrowUp, MdKeyboardArrowDown} from 'react-icons/md';
+interface OrdinationProps {
+    ordination: string;
+    setOrdination: React.Dispatch<React.SetStateAction<string>>;
+}
 
-
-export default function Ordination() {
+export default function Ordination({ordination, setOrdination}: OrdinationProps) {
     const [open, setOpen] = useState(false);
+    const nameOrdination = options.find(option => option.value === ordination)?.nome;
+
     return (
         <button 
-        className={styles.ordination} 
+        className={classNames({
+            [styles.ordination]: true,
+            [styles['ordination--active']]: ordination !== '',
+        })} 
         onClick={() => setOpen(!open)}
         onBlur={() => setOpen(false)}
         >
-            <span>Ordenar por</span>
+            <span>{nameOrdination || 'Ordenar por'}</span>
             { open ? <MdKeyboardArrowUp size={20}/> : <MdKeyboardArrowDown size={20}/> }
             <div className={classNames({
                 [styles.ordination__options] : true,
                 [styles["ordination__options--active"]] : open
             })}>
                 {options.map((option) => (
-                    <div className={styles.ordination__option} key={option.value}>{option.nome}</div>
+                    <div 
+                    className={styles.ordination__option} 
+                    key={option.value}
+                    onClick={() => setOrdination(option.value)}
+                    >{option.nome}</div>
                 ))}
             </div>
         </button>
