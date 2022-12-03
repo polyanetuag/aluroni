e2a623a55c89096fb190ecc8ae07ac276ceee922
Polyane Tuag <pollyportugues@hotmail.com>
2022-12-03 11:04:06 -0300

    feat: Itens component

---
+++ src/pages/Menu/Itens/index.tsx
@@ -0,0 +1,13 @@
+import menu from "./itens.json";
+import Item from "./Item";
+import styles from "./Itens.module.scss";
+
+export default function Itens() {
+  return (
+    <div className={styles.itens}>
+      {menu.map((item) => (
+        <Item key={item.id}/>
+      ))}
+    </div>
+  );
+}
