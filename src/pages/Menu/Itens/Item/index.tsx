1aa706f54626513ae7ecccc4e2cc3341150d0174
Polyane Tuag <pollyportugues@hotmail.com>
2022-12-04 12:02:44 -0300

    fix: dinamic Item component

--- src/pages/Menu/Itens/Item/index.tsx
+++ src/pages/Menu/Itens/Item/index.tsx
@@ -1,25 +1,28 @@
 import logo from "assets/logo.svg";
 import styles from "./Item.module.scss";
+import menu from "../itens.json";
 
-export default function Item() {
+type ItemProps = typeof menu[0];
+
+export default function Item(props: ItemProps) {
+  const { title, description, category,size,serving, price } = props;
   return (
     <div className={styles.item}>
       <div className={styles.item__imagem}>
-        <img src={logo} alt="imagem" />
+        <img src={logo} alt={title} />
       </div>
 
       <div className={styles.item__descricao}>
         <div className={styles.item__titulo}>
-          <h2>Nome do item</h2>
-          <p>Descrição do item</p>
+          <h2>{title}</h2>
+          <p>{description}</p>
         </div>
 
         <div className={styles.item__tags}>
-            <div className={styles.item__tipo}>MASSA</div>
-            <div className={styles.item__porcao}>500g</div>
-            <div className={styles.item__qtdpessoas}> Serve 2 pessoas</div>
-            <div className={styles.item__valor}>R$ 20,00</div>
-
+          <div className={styles.item__tipo}>{category.label}</div>
+          <div className={styles.item__porcao}>{size}g</div>
+          <div className={styles.item__qtdpessoas}> Serve {serving} pessoa{serving === 1 ? '': 's'}</div>
+          <div className={styles.item__valor}>R$ {price.toFixed(2)}</div>
         </div>
       </div>
     </div>
