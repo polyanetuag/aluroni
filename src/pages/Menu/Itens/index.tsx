c77877c7224eae76eb9e296eb925e785ad0ffe67
Polyane Tuag <pollyportugues@hotmail.com>
2022-12-06 20:15:45 -0300

    feat: function search

--- src/pages/Menu/Itens/index.tsx
+++ src/pages/Menu/Itens/index.tsx
@@ -1,12 +1,37 @@
 import menu from "./itens.json";
 import Item from "./Item";
 import styles from "./Itens.module.scss";
+import { useEffect, useState } from "react";
 
-export default function Itens() {
+interface ItensProps {
+  search: string;
+  filter: number | null;
+  ordination: string;
+}
+
+export default function Itens(props: ItensProps) {
+  const [list, setList] = useState(menu);
+  const { search, filter } = props;
+
+  function testSearch(title: string) {
+    const regex = new RegExp(search, "i");
+    return regex.test(title);
+  }
+
+  function testFilter(id: number) {
+    if(filter !== null) return filter === id;
+    return true;
+  }
+
+  useEffect(() => {
+    const newList = menu.filter(item=> testSearch(item.title) && testFilter(item.category.id))
+    setList(newList);
+  }, [search, filter]);
+
   return (
     <div className={styles.itens}>
-      {menu.map((item) => (
-        <Item key={item.id} {...item}/>
+      {list.map((item) => (
+        <Item key={item.id} {...item} />
       ))}
     </div>
   );
