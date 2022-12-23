141dc123fe06ac7eb9ab7cca5c4bf79f5e71f172
Polyane Tuag <pollyportugues@hotmail.com>
2022-12-23 17:01:24 -0300

    feat: component Home

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,29 @@
+import menu from "data/menu.json";
+import styles from "./Home.module.scss";
+
+export default function Home() {
+  let recommendedDishes = [...menu];
+  recommendedDishes = recommendedDishes
+    .sort(() => 0.5 - Math.random())
+    .splice(0, 3);
+
+  return (
+    <section>
+        <h3 className={styles.titulo}>
+            Recomendações da cozinha
+        </h3>
+        <div className={styles.recomendados}>
+            {recommendedDishes.map(item => (
+                <div key={item.id} className={styles.recomendado}>
+                    <div className={styles.recomendado__imagem}>
+                        <img src={item.photo} alt={item.title} />
+                    </div>
+                    <button className={styles.recomendado__botao}>
+                        Ver mais
+                    </button>
+                </div>
+            ))} 
+        </div>
+    </section>
+  );
+}
