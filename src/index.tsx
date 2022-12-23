9716d58604511baa849b854d0ea03bab9874a954
Polyane Tuag <pollyportugues@hotmail.com>
2022-12-23 17:01:07 -0300

    fix: remove Menu component

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'normalize.css'
 import './index.css';
-import Menu from './pages/Menu';
+import Home from 'pages/Home';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Menu />
+    <Home />
   </React.StrictMode>
 );
 
