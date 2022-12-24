15f57edbea86c52b6b76c9bc619f5809d88df4d2
Polyane Tuag <pollyportugues@hotmail.com>
2022-12-24 17:18:17 -0300

    feat: route index

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client';
 import 'normalize.css'
 import './index.css';
 import Home from 'pages/Home';
+import Menu from 'pages/Menu';
+
+const componenteAtual = window.location.pathname === '/' ? <Home/> : <Menu/>;
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Home />
+    {componenteAtual}
   </React.StrictMode>
 );
 
