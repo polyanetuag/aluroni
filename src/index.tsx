270525476dc6f7d5f0c0974622e1122f71ddb353
Polyane Tuag <pollyportugues@hotmail.com>
2022-12-25 10:52:51 -0300

    feat: add router

--- src/index.tsx
+++ src/index.tsx
@@ -2,17 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'normalize.css'
 import './index.css';
-import Home from 'pages/Home';
-import Menu from 'pages/Menu';
 
-const componenteAtual = window.location.pathname === '/' ? <Home/> : <Menu/>;
+import Router from './routes'
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    {componenteAtual}
+    <Router />
   </React.StrictMode>
 );
 
