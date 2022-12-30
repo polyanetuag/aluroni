df09c50364bfeb84c67dfe5cfb1049879f14dfbb
Polyane Tuag <pollyportugues@hotmail.com>
2022-12-30 18:18:14 -0300

    feat: routes MenuBoard

--- src/components/MenuBoard/index.tsx
+++ src/components/MenuBoard/index.tsx
@@ -2,9 +2,27 @@ import { ReactComponent as Logo } from 'assets/logo.svg';
 import styles from './MenuBoard.module.scss';
 
 export default function MenuBoard() {
+  const routes = [
+    { label: 'Início',
+     to: '/' 
+    }, 
+    { label: 'Cardápio',
+     to: '/cardapio' 
+    },
+    { label: 'Sobre',
+     to: '/sobre'
+    },];
+
   return (
     <nav className={styles.menu}>
           <Logo />
+          <ul className={styles.menu__list}>
+            {routes.map((route, index) => (
+              <li key={index} className={styles.menu__link}>
+                <a href={route.to}>{route.label}</a>
+              </li>
+            ))}
+          </ul>
         </nav>
   );
 }
