4755ff1dd676e6bd904d10644f68e64d19e2450b
Polyane Tuag <pollyportugues@hotmail.com>
2023-01-01 20:16:50 -0300

    refactor: remove header

--- src/pages/Menu/index.tsx
+++ src/pages/Menu/index.tsx
@@ -1,29 +1,24 @@
-import styles from './Menu.module.scss';
-import Search from './Search';
-import { useState } from 'react';
-import Filters from './Filters';
-import Ordination from './Ordination';
-import Itens from './Itens';
+import styles from "./Menu.module.scss";
+import Search from "./Search";
+import { useState } from "react";
+import Filters from "./Filters";
+import Ordination from "./Ordination";
+import Itens from "./Itens";
 
 export default function Menu() {
-const [search, setSearch] = useState("");
-const [filter, setFilter] = useState<number | null>(null);
-const [ordination, setOrdination] = useState('');
+  const [search, setSearch] = useState("");
+  const [filter, setFilter] = useState<number | null>(null);
+  const [ordination, setOrdination] = useState("");
 
   return (
-    <main>
-        <header className={styles.header}>
-          <div className={styles.header__text}> A casa do código e da massa</div>
-        </header>
-        <section className={styles.menu}>
-          <h3 className={styles.menu__title}>Cardápio</h3>
-          <Search search={search} setSearch={setSearch} />
-          <div className={styles.menu__filters}>
-            <Filters filter={filter} setFilter={setFilter} />
-            <Ordination ordination={ordination} setOrdination={setOrdination} />
-          </div>
-          <Itens search={search} filter={filter} ordination={ordination} />
-        </section>
-    </main>
-  )
+    <section className={styles.menu}>
+      <h3 className={styles.menu__title}>Cardápio</h3>
+      <Search search={search} setSearch={setSearch} />
+      <div className={styles.menu__filters}>
+        <Filters filter={filter} setFilter={setFilter} />
+        <Ordination ordination={ordination} setOrdination={setOrdination} />
+      </div>
+      <Itens search={search} filter={filter} ordination={ordination} />
+    </section>
+  );
 }
