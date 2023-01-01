dff13a4888d2f7536ff5276304cad29f19559db8
Polyane Tuag <pollyportugues@hotmail.com>
2023-01-01 20:16:09 -0300

    feat: add header route

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,22 @@
-import MenuBoard from 'components/MenuBoard';
-import Menu from 'pages/Menu';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import Home from './pages/Home';
+import MenuBoard from "components/MenuBoard";
+import Menu from "pages/Menu";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import styles from "./Routes.module.css";
 
 export default function AppRouter() {
   return (
-    <Router>
-      <MenuBoard />
+    <main>
+      <Router>
+        <MenuBoard />
+        <header className={styles.header}>
+          <div className={styles.header__text}> A casa do código e da massa</div>
+        </header>
         <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/menu" element={<Menu />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/menu" element={<Menu />} />
         </Routes>
-    </Router>
+      </Router>
+    </main>
   );
 }
