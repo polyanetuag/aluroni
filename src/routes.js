c0201246275fb0b9809b413f1e6ddc1bd1d03915
Polyane Tuag <pollyportugues@hotmail.com>
2023-01-02 09:23:26 -0300

    feat: add route Default Page

--- src/routes.js
+++ src/routes.js
@@ -1,21 +1,20 @@
+import DefaultPage from "components/DefaultPage";
 import MenuBoard from "components/MenuBoard";
 import Menu from "pages/Menu";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
-import styles from "./Routes.module.css";
 
 export default function AppRouter() {
   return (
     <main>
       <Router>
         <MenuBoard />
-        <header className={styles.header}>
-          <div className={styles.header__text}> A casa do código e da massa</div>
-        </header>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/menu" element={<Menu />} />
-        </Routes>
+          <Route path='/' element={<DefaultPage/>}>
+            <Route index element={<Home />} />
+            <Route path='cardapio' element={<Menu />}/>
+          </Route>
+      </Routes>
       </Router>
     </main>
   );
