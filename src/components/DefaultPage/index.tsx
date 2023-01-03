ea2f11d4fd016f46db903a908204b7e5c316b0f5
Polyane Tuag <pollyportugues@hotmail.com>
2023-01-03 08:47:55 -0300

    feat: add className stylesTheme

--- src/components/DefaultPage/index.tsx
+++ src/components/DefaultPage/index.tsx
@@ -1,5 +1,6 @@
 import styles from "./DefaultPage.module.scss";
 import { Outlet } from "react-router-dom";
+import stylesTheme from "styles/Theme.module.scss";
 
 export default function DefaultPage() {
   return (
@@ -7,7 +8,7 @@ export default function DefaultPage() {
       <header className={styles.header}>
         <div className={styles.header__text}> A casa do código e da massa</div>
       </header>
-      <div>
+      <div className={stylesTheme.container}>
         <Outlet />
       </div>
     </>
