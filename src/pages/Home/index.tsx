92bc0a37cffabe90537e624db5201f1cf72be293
Polyane Tuag <pollyportugues@hotmail.com>
2023-01-04 19:16:15 -0300

    feat: add info about address

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,8 @@
 import menu from "data/menu.json";
 import styles from "./Home.module.scss";
 import stylesTheme from "styles/Theme.module.scss";
+import nossaCasa from 'assets/nossa_casa.png';
+import React from "react";
 
 export default function Home() {
   let recommendedDishes = [...menu];
@@ -24,6 +26,13 @@ export default function Home() {
                     </button>
                 </div>
             ))} 
+        </div>
+        <h3 className={stylesTheme.titulo}>Nossa casa</h3>
+        <div className={styles.nossaCasa}>
+            <img src={nossaCasa} alt="Casa do Aluroni" />
+            <div className={styles.nossaCasa__endereco}>
+                Rua Aveiro Dias, 1234 <br /> <br /> São Paulo - SP
+            </div>
         </div>
     </section>
   );
