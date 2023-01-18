326f84d0bd537f130987ce258a74bc73a1605378
Polyane Tuag <pollyportugues@hotmail.com>
2023-01-18 16:08:37 -0300

    fix: create route for plate component

--- src/pages/Plate/index.tsx
+++ src/pages/Plate/index.tsx
@@ -1,8 +1,9 @@
 import styles from "./Plate.module.scss";
-import { useParams, useNavigate } from "react-router-dom";
+import { useParams, useNavigate, Routes, Route } from "react-router-dom";
 import menu from "data/menu.json";
 import TagsPlate from "components/TagsPlate";
 import NotFound from "pages/NotFound";
+import DefaultPage from "components/DefaultPage";
 
 export default function Plate() {
   const { id } = useParams();
@@ -12,18 +13,31 @@ export default function Plate() {
     return <NotFound />;
   }
   return (
-    <>
-      <button className={styles.voltar} onClick={() => navigate(-1)}>{"< Voltar"}</button>
-      <section className={styles.container}>
-        <h1 className={styles.titulo}>{plate.title}</h1>
-        <div className={styles.imagem}>
-          <img src={plate.photo} alt={plate.title} />
-        </div>
-        <div className={styles.conteudo}>
-          <p className={styles.conteudo__descricao}>{plate.description}</p>
-          <TagsPlate {...plate} />
-        </div>
-      </section>
-    </>
+    <Routes>
+      <Route path="*" element={<DefaultPage />}>
+        <Route
+          index
+          element={
+            <>
+              <button className={styles.voltar} onClick={() => navigate(-1)}>
+                {"< Voltar"}
+              </button>
+              <section className={styles.container}>
+                <h1 className={styles.titulo}>{plate.title}</h1>
+                <div className={styles.imagem}>
+                  <img src={plate.photo} alt={plate.title} />
+                </div>
+                <div className={styles.conteudo}>
+                  <p className={styles.conteudo__descricao}>
+                    {plate.description}
+                  </p>
+                  <TagsPlate {...plate} />
+                </div>
+              </section>
+            </>
+          }
+        ></Route>
+      </Route>
+    </Routes>
   );
 }
